ac180748e2757fbc7fc918c41fd2087b23faaaaa
Grizzly <punxrules666@gmail.com>
2024-10-01 01:45:45 +0300

    removed repeated code

--- alad/alad.py
+++ alad/alad.py
@@ -7,7 +7,7 @@ import os
 import tensorflow as tf
 import numpy as np
 from sklearn import metrics
-from tensorflow.keras.utils import plot_model
+# from tensorflow.keras.utils import plot_model
 from tqdm import tqdm
 import pandas as pd
 
@@ -49,6 +49,29 @@ class ALAD:
         self.batch_size = batch_size
         self.learning_rate = models_module.LEARNING_RATE
 
+        # Create models
+        self.models = {"gen": {"model": models_module.Generator(random_seed)},
+                       "enc": {"model": models_module.Encoder(random_seed)},
+                       "dis_xz": {"model": models_module.DiscriminatorXZ(random_seed)},
+                       "dis_xx": {"model": models_module.DiscriminatorXX(random_seed)},
+                       "dis_zz": {"model": models_module.DiscriminatorZZ(random_seed)}}
+
+        # Build models
+        for model_dict in self.models.values():
+            model_dict["model"].build(batch_size=batch_size)
+
+        # Optimizers
+        for model_dict in self.models.values():
+            model_dict["optimizer"] = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
+
+        # ExponentialMovingAverage (EMA)
+        for model_dict in self.models.values():
+            model_dict["ema"] = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
+
+        # Apply EMA for the first time after building models
+        for model_dict in self.models.values():
+            model_dict["ema"].apply([var.value for var in model_dict["model"].trainable_variables])
+
         # Create results directory
         self.results_dir = create_results_dir(dataset=dataset_name, allow_zz=allow_zz, random_seed=random_seed)
         # Create logger
@@ -58,82 +81,34 @@ class ALAD:
         print_parameters(self.logger, **{"dataset": dataset_name, "allow_zz": allow_zz, "random_seed": random_seed,
                                          "batch_size": batch_size})
 
-        # Models
-        self.gen = models_module.Generator(random_seed)
-        self.enc = models_module.Encoder(random_seed)
-        self.dis_xz = models_module.DiscriminatorXZ(random_seed)
-        self.dis_xx = models_module.DiscriminatorXX(random_seed)
-        self.dis_zz = models_module.DiscriminatorZZ(random_seed)
-
-        # Build models
-        self.gen.build(batch_size=batch_size)
-        self.enc.build(batch_size=batch_size)
-        self.dis_xz.build(batch_size=batch_size)
-        self.dis_xx.build(batch_size=batch_size)
-        self.dis_zz.build(batch_size=batch_size)
-
-        # Optimizers
-        self.optimizer_gen = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
-        self.optimizer_enc = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
-        self.optimizer_dis_xz = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
-        self.optimizer_dis_xx = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
-        self.optimizer_dis_zz = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=0.5)
-
-        # ExponentialMovingAverage (EMA)
-        self.gen_ema = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
-        self.enc_ema = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
-        self.xz_ema = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
-        self.xx_ema = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
-        self.zz_ema = tf.train.ExponentialMovingAverage(decay=self.ema_decay)
-
-        # Apply EMA after building models
-        self.gen_ema.apply([var.value for var in self.gen.trainable_variables])
-        self.enc_ema.apply([var.value for var in self.enc.trainable_variables])
-        self.xz_ema.apply([var.value for var in self.dis_xz.trainable_variables])
-        self.xx_ema.apply([var.value for var in self.dis_xx.trainable_variables])
-        self.zz_ema.apply([var.value for var in self.dis_zz.trainable_variables])
-
-        # Temporary buffer for saving actual weights when swapping them to EMA average
-        self.vars_tmp = {}
-
         # Loss history
         self.loss_hist = {"epoch": [], "gen": [], "enc": [], "dis": [], "dis_xz": [], "dis_xx": [], "dis_zz": []}
 
         self.epochs_done = 0
+        self.is_ema_swapped = False  # are actual weights swapped with EMA weigts
 
     def swap_vars(self):
         """
         Swap actual weights and EMA weights or, if it was swapped, swap back
         """
-        if len(self.vars_tmp) == 0:
-            # Save variables
-            self.vars_tmp["gen"] = self.gen.get_weights()
-            self.vars_tmp["enc"] = self.enc.get_weights()
-            self.vars_tmp["dis_xz"] = self.dis_xz.get_weights()
-            self.vars_tmp["dis_xx"] = self.dis_xx.get_weights()
-            self.vars_tmp["dis_zz"] = self.dis_zz.get_weights()
-
-            # Load EMA averages
-            for var in self.gen.trainable_variables:
-                var.value.assign(self.gen_ema.average(var.value))
-            for var in self.enc.trainable_variables:
-                var.value.assign(self.enc_ema.average(var.value))
-            for var in self.dis_xz.trainable_variables:
-                var.value.assign(self.xz_ema.average(var.value))
-            for var in self.dis_xx.trainable_variables:
-                var.value.assign(self.xx_ema.average(var.value))
-            for var in self.dis_zz.trainable_variables:
-                var.value.assign(self.zz_ema.average(var.value))
-        else:
+        if self.is_ema_swapped:
             # Load saved variables
-            self.gen.set_weights(self.vars_tmp["gen"])
-            self.enc.set_weights(self.vars_tmp["enc"])
-            self.dis_xz.set_weights(self.vars_tmp["dis_xz"])
-            self.dis_xx.set_weights(self.vars_tmp["dis_xx"])
-            self.dis_zz.set_weights(self.vars_tmp["dis_zz"])
-
-            # Delete them from buffer
-            self.vars_tmp = {}
+            for model_name in self.models:
+                self.models[model_name]["model"].set_weights(
+                    self.models[model_name]["vars_tmp"]
+                )
+                del self.models[model_name]["vars_tmp"]
+            self.is_ema_swapped = False
+        else:
+            # TODO here is
+            # Save weights
+            for model_name in self.models:
+                self.models[model_name]["vars_tmp"] = self.models[model_name]["model"].get_weights()
+            # Swap to EMA weights
+            for model_name in self.models:
+                for var in self.models[model_name]["model"].trainable_variables:
+                    var.value.assign(self.models[model_name]["ema"].average(var.value))
+            self.is_ema_swapped = True
 
     def train(self, trainx, epochs: int):
         self.logger.info(f"Training epochs {0} - {epochs - 1}")
@@ -146,9 +121,11 @@ class ALAD:
 
         for epoch in range(epochs):
             # Epoch losses
-            train_loss_dis_xz, train_loss_dis_xx, train_loss_dis_zz, train_loss_dis, \
-                train_loss_gen, train_loss_enc = [0, 0, 0, 0, 0, 0]
+            epoch_losses = {"gen": 0, "enc": 0, "dis": 0, "dis_xz": 0, "dis_xx": 0, "dis_zz": 0}
+            # train_loss_dis_xz, train_loss_dis_xx, train_loss_dis_zz, train_loss_dis, \
+            #     train_loss_gen, train_loss_enc = [0, 0, 0, 0, 0, 0]
             start_time = time.time()
+
             for step in tqdm(range(n_batches), desc=f"Epoch {epoch}", file=sys.stdout):
                 idx_from = step * self.batch_size
                 idx_to = (step + 1) * self.batch_size
@@ -158,39 +135,32 @@ class ALAD:
 
                 ld, ldxz, ldxx, ldzz, le, lg = self.train_step(x_batch, z_batch)
 
-                train_loss_dis += ld
-                train_loss_dis_xz += ldxz
-                train_loss_dis_xx += ldxx
-                train_loss_dis_zz += ldzz
-                train_loss_gen += lg
-                train_loss_enc += le
+                epoch_losses["gen"] += lg
+                epoch_losses["enc"] += le
+                epoch_losses["dis"] += ld
+                epoch_losses["dis_xz"] += ldxz
+                epoch_losses["dis_xx"] += ldxx
+                epoch_losses["dis_zz"] += ldzz
 
-            train_loss_gen /= n_batches
-            train_loss_enc /= n_batches
-            train_loss_dis /= n_batches
-            train_loss_dis_xz /= n_batches
-            train_loss_dis_xx /= n_batches
-            train_loss_dis_zz /= n_batches
+            for key in epoch_losses:
+                epoch_losses[key] /= n_batches
 
             self.loss_hist["epoch"].append(epoch)
-            self.loss_hist["gen"].append(tf.get_static_value(train_loss_gen))
-            self.loss_hist["enc"].append(tf.get_static_value(train_loss_enc))
-            self.loss_hist["dis"].append(tf.get_static_value(train_loss_dis))
-            self.loss_hist["dis_xz"].append(tf.get_static_value(train_loss_dis_xz))
-            self.loss_hist["dis_xx"].append(tf.get_static_value(train_loss_dis_xx))
-            self.loss_hist["dis_zz"].append(tf.get_static_value(train_loss_dis_zz))
+            for key in epoch_losses:
+                self.loss_hist[key].append(tf.get_static_value(epoch_losses[key]))
 
             epoch_time = time.time() - start_time
             if self.allow_zz:
                 self.logger.info(
-                    f"time = {epoch_time:.2f}s | loss gen = {train_loss_gen:.4f} | loss enc = {train_loss_enc:.4f} | "
-                    f"loss dis = {train_loss_dis:.4f} | loss dis xz = {train_loss_dis_xz:.4f} | "
-                    f"loss dis xx = {train_loss_dis_xx:.4f} | loss dis zz = {train_loss_dis_zz:.4f} |")
+                    f"epoch {epoch} | time = {epoch_time:.2f}s | loss gen = {epoch_losses['gen']:.4f} | "
+                    f"loss enc = {epoch_losses['enc']:.4f} | loss dis = {epoch_losses['dis']:.4f} | "
+                    f"loss dis xz = {epoch_losses['dis_xz']:.4f} | loss dis xx = {epoch_losses['dis_xx']:.4f} | "
+                    f"loss dis zz = {epoch_losses['dis_zz']:.4f} |")
             else:
                 self.logger.info(
-                    f"time = {epoch_time:.2f}s | loss gen = {train_loss_gen:.4f} | loss enc = {train_loss_enc:.4f} | "
-                    f"loss dis = {train_loss_dis:.4f} | loss dis xz = {train_loss_dis_xz:.4f} | "
-                    f"loss dis xx = {train_loss_dis_xx:.4f} |")
+                    f"epoch {epoch} | time = {epoch_time:.2f}s | loss gen = {epoch_losses['gen']:.4f} | "
+                    f"loss enc = {epoch_losses['enc']:.4f} | loss dis = {epoch_losses['dis']:.4f} | "
+                    f"loss dis xz = {epoch_losses['dis_xz']:.4f} | loss dis xx = {epoch_losses['dis_xx']:.4f} |")
 
             self.epochs_done += 1
 
@@ -204,22 +174,22 @@ class ALAD:
 
         ### Train discriminators ###
         with tf.GradientTape(persistent=True) as tape:
-            z_gen = self.enc(x_pl, training=True)
-            x_gen = self.gen(z_pl, training=True)
-            rec_x = self.gen(z_gen, training=True)
-            rec_z = self.enc(x_gen, training=True)
+            z_gen = self.models["enc"]["model"](x_pl, training=True)
+            x_gen = self.models["gen"]["model"](z_pl, training=True)
+            rec_x = self.models["gen"]["model"](z_gen, training=True)
+            rec_z = self.models["enc"]["model"](x_gen, training=True)
 
             # D(x, z)
-            l_encoder, inter_layer_inp_xz = self.dis_xz(x_pl, z_gen, training=True)
-            l_generator, inter_layer_rct_xz = self.dis_xz(x_gen, z_pl, training=True)
+            l_encoder, inter_layer_inp_xz = self.models["dis_xz"]["model"](x_pl, z_gen, training=True)
+            l_generator, inter_layer_rct_xz = self.models["dis_xz"]["model"](x_gen, z_pl, training=True)
 
             # D(x, x)
-            x_logit_real, inter_layer_inp_xx = self.dis_xx(x_pl, x_pl, training=True)
-            x_logit_fake, inter_layer_rct_xx = self.dis_xx(x_pl, rec_x, training=True)
+            x_logit_real, inter_layer_inp_xx = self.models["dis_xx"]["model"](x_pl, x_pl, training=True)
+            x_logit_fake, inter_layer_rct_xx = self.models["dis_xx"]["model"](x_pl, rec_x, training=True)
 
             # D(z, z)
-            z_logit_real, _ = self.dis_zz(z_pl, z_pl, training=True)
-            z_logit_fake, _ = self.dis_zz(z_pl, rec_z, training=True)
+            z_logit_real, _ = self.models["dis_zz"]["model"](z_pl, z_pl, training=True)
+            z_logit_fake, _ = self.models["dis_zz"]["model"](z_pl, rec_z, training=True)
 
             ### LOSSES ###
 
@@ -249,39 +219,42 @@ class ALAD:
         ld, ldxz, ldxx, ldzz = loss_discriminator, dis_loss_xz, dis_loss_xx, dis_loss_zz
 
         # Calculate discriminators gradients
-        dis_xz_grad = tape.gradient(dis_loss_xz, self.dis_xz.trainable_variables)
-        dis_xx_grad = tape.gradient(dis_loss_xx, self.dis_xx.trainable_variables)
-        dis_zz_grad = tape.gradient(dis_loss_zz, self.dis_zz.trainable_variables)
+        dis_xz_grad = tape.gradient(dis_loss_xz, self.models["dis_xz"]["model"].trainable_variables)
+        dis_xx_grad = tape.gradient(dis_loss_xx, self.models["dis_xx"]["model"].trainable_variables)
+        dis_zz_grad = tape.gradient(dis_loss_zz, self.models["dis_zz"]["model"].trainable_variables)
         del tape
 
         # Update discriminators weights
-        self.optimizer_dis_xz.apply_gradients(zip(dis_xz_grad, self.dis_xz.trainable_variables))
-        self.optimizer_dis_xx.apply_gradients(zip(dis_xx_grad, self.dis_xx.trainable_variables))
-        self.optimizer_dis_zz.apply_gradients(zip(dis_zz_grad, self.dis_zz.trainable_variables))
+        self.models["dis_xz"]["optimizer"].apply_gradients(
+            zip(dis_xz_grad, self.models["dis_xz"]["model"].trainable_variables))
+        self.models["dis_xx"]["optimizer"].apply_gradients(
+            zip(dis_xx_grad, self.models["dis_xx"]["model"].trainable_variables))
+        self.models["dis_zz"]["optimizer"].apply_gradients(
+            zip(dis_zz_grad, self.models["dis_zz"]["model"].trainable_variables))
 
         # Update EMA
-        self.xz_ema.apply([var.value for var in self.dis_xz.trainable_variables])
-        self.xx_ema.apply([var.value for var in self.dis_xx.trainable_variables])
-        self.zz_ema.apply([var.value for var in self.dis_zz.trainable_variables])
+        discriminators = ["dis_xz", "dis_xx", "dis_zz"]
+        for discr in discriminators:
+            self.models[discr]["ema"].apply([var.value for var in self.models[discr]["model"].trainable_variables])
 
         ### Train generator and encoder ###
         with tf.GradientTape(persistent=True) as tape:
-            z_gen = self.enc(x_pl, training=True)
-            x_gen = self.gen(z_pl, training=True)
-            rec_x = self.gen(z_gen, training=True)
-            rec_z = self.enc(x_gen, training=True)
+            z_gen = self.models["enc"]["model"](x_pl, training=True)
+            x_gen = self.models["gen"]["model"](z_pl, training=True)
+            rec_x = self.models["gen"]["model"](z_gen, training=True)
+            rec_z = self.models["enc"]["model"](x_gen, training=True)
 
             # D(x, z)
-            l_encoder, inter_layer_inp_xz = self.dis_xz(x_pl, z_gen, training=True)
-            l_generator, inter_layer_rct_xz = self.dis_xz(x_gen, z_pl, training=True)
+            l_encoder, inter_layer_inp_xz = self.models["dis_xz"]["model"](x_pl, z_gen, training=True)
+            l_generator, inter_layer_rct_xz = self.models["dis_xz"]["model"](x_gen, z_pl, training=True)
 
             # D(x, x)
-            x_logit_real, inter_layer_inp_xx = self.dis_xx(x_pl, x_pl, training=True)
-            x_logit_fake, inter_layer_rct_xx = self.dis_xx(x_pl, rec_x, training=True)
+            x_logit_real, inter_layer_inp_xx = self.models["dis_xx"]["model"](x_pl, x_pl, training=True)
+            x_logit_fake, inter_layer_rct_xx = self.models["dis_xx"]["model"](x_pl, rec_x, training=True)
 
             # D(z, z)
-            z_logit_real, _ = self.dis_zz(z_pl, z_pl, training=True)
-            z_logit_fake, _ = self.dis_zz(z_pl, rec_z, training=True)
+            z_logit_real, _ = self.models["dis_zz"]["model"](z_pl, z_pl, training=True)
+            z_logit_fake, _ = self.models["dis_zz"]["model"](z_pl, rec_z, training=True)
 
             ### LOSSES ###
 
@@ -332,17 +305,17 @@ class ALAD:
         le, lg = loss_encoder, loss_generator
 
         # Calculate encoder and generator gradients
-        gen_grad = tape.gradient(loss_generator, self.gen.trainable_variables)
-        enc_grad = tape.gradient(loss_encoder, self.enc.trainable_variables)
+        gen_grad = tape.gradient(loss_generator, self.models["gen"]["model"].trainable_variables)
+        enc_grad = tape.gradient(loss_encoder, self.models["enc"]["model"].trainable_variables)
         del tape
 
         # Update gen and enc weights
-        self.optimizer_gen.apply_gradients(zip(gen_grad, self.gen.trainable_variables))
-        self.optimizer_enc.apply_gradients(zip(enc_grad, self.enc.trainable_variables))
+        self.models["gen"]["optimizer"].apply_gradients(zip(gen_grad, self.models["gen"]["model"].trainable_variables))
+        self.models["enc"]["optimizer"].apply_gradients(zip(enc_grad, self.models["enc"]["model"].trainable_variables))
 
         # Update EMA
-        self.gen_ema.apply([var.value for var in self.gen.trainable_variables])
-        self.enc_ema.apply([var.value for var in self.enc.trainable_variables])
+        self.models["gen"]["ema"].apply([var.value for var in self.models["gen"]["model"].trainable_variables])
+        self.models["enc"]["ema"].apply([var.value for var in self.models["enc"]["model"].trainable_variables])
 
         return ld, ldxz, ldxx, ldzz, le, lg
 
@@ -442,12 +415,12 @@ class ALAD:
     # TODO add
     @tf.function
     def test_step(self, x_pl, z_pl, degree):
-        z_gen_ema = self.enc(x_pl, training=False)
-        rec_x_ema = self.gen(z_gen_ema, training=False)
-        x_gen_ema = self.gen(z_pl, training=False)
+        z_gen_ema = self.models["enc"]["model"](x_pl, training=False)
+        rec_x_ema = self.models["gen"]["model"](z_gen_ema, training=False)
+        # x_gen_ema = self.gen(z_pl, training=False)
 
-        l_encoder_emaxx, inter_layer_inp_emaxx = self.dis_xx(x_pl, x_pl, training=False)
-        l_generator_emaxx, inter_layer_rct_emaxx = self.dis_xx(x_pl, rec_x_ema, training=False)
+        l_encoder_emaxx, inter_layer_inp_emaxx = self.models["dis_xx"]["model"](x_pl, x_pl, training=False)
+        l_generator_emaxx, inter_layer_rct_emaxx = self.models["dis_xx"]["model"](x_pl, rec_x_ema, training=False)
 
         _score_ch = score_ch(l_generator_emaxx)
         _score_l1 = score_l1(x_pl, rec_x_ema)
